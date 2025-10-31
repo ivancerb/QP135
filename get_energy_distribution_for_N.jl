19c47645c1152700387a92f2a81e43e90c2d07c6
Ivan de Campos Cerboncini <ivanszcer@gmail.com>
2025-10-31 19:16:47 -0300

    script to calculate energy distribution for N particles - ex. 73 - 75

---
+++ get_energy_distribution_for_N.jl
@@ -0,0 +1,68 @@
+#Ex 75
+#Calculates the number of microstates corresponding to a giving total energy, for a number of particles N
+#System can have from 1 up to 6 particles
+#Each particle can have energies in the range 1-6
+#N is the number of particles of the system
+#Syntax:
+#julia get_energy_distribution_for_N.jl N 
+
+import Pkg; 
+
+Pkg.add("DataStructures")
+
+using DataStructures
+
+energy_map = SortedDict{Int, Int}() #key = energy of the system; value = number of microstates with that energy
+
+function count_energy(E, energy_map)
+	if haskey(energy_map, E)
+		energy_map[E] = energy_map[E] + 1
+	else
+		energy_map[E] = 1
+	end
+end
+
+function nestedLoops(pos, N, vec, num_p, energy_map)
+	if pos == N+1
+        	#print(vec)
+		E=sum(vec)
+		#println(" E=$E")
+		count_energy(E, energy_map)
+		num_p[1]+=1
+        	return
+	end
+#	if pos == N
+#		println()
+#	end
+	for i in (1:6)
+        	vec[pos] = i
+        	nestedLoops(pos+1, N, vec, num_p, energy_map)
+	end
+end
+
+function extract_energy_data_to_file(energy_map, N)
+	#println("@title \"Distribuição de microestados - $N partículas\"")
+	#println("@xaxis label \"Energia (unidades arbitrárias)\"")
+	#println("@yaxis label \"Número de microestados\"")
+	for (key, value) in energy_map
+		println("$key $value")
+	end
+end
+
+N=parse(Int, ARGS[1])
+
+#println("Energies combinations - $N PARTICLE(S) ###########") 	
+
+num_p=zeros(Int64,1)
+vec = ones(Int64,N)
+nestedLoops(1, N, vec, num_p, energy_map)
+
+#println()
+#println("Total number of energy combinations for each particle - Distinguishable case")
+#println()
+
+
+#println("$N PARTICLE(S): $(num_p[1]) POSSIBILITIES")
+
+#println("Energy map")
+extract_energy_data_to_file(energy_map, N)
